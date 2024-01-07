eb9961d886866012d4b03fd4b46b2fbf551a4a62
Luis Topia <cometothecabaret@hotmail.com>
2024-01-07 19:12:59 +0000

    react quiz

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+
+import questions from './questions';
+import {useState, useEffect} from 'react';
+
 
 function App() {
+
+  const [actualQuestion, setActualQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [isFinished, setIsFinished] = useState(false);
+  const [isTimeRunning, setIsTimeRunning] = useState(10);
+  const [areDisabled, setAreDisabled] =useState(false);
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  function handleAnswerSubmit(isCorrect, e){
+    //add score
+   if(isCorrect) setScore(score + 1);
+    //add styles
+   e.target.classList.add(isCorrect ? "correct" : "incorrect");
+    //move to next question 
+   setTimeout(()=>{
+    if(actualQuestion === questions.length -1){
+      setIsFinished(true);
+    }else{
+      setActualQuestion(actualQuestion + 1);
+    }
+   }, 1500)
+  }
+  useEffect(()=> {
+    const interval = setInterval(() => {
+      if(isTimeRunning > 0 ) setIsTimeRunning((prev) => prev - 1);
+      if(isTimeRunning ===0) setAreDisabled(true);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [isTimeRunning]);
+  if(isFinished) return(
+    <main className="app">
+      <div className="finish-game">
+        <span> You Got {score} Right Answer of {questions.length} Questions </span>
+        <button onClick={()=> (window.location.href ="/")}>Play Again.</button>
+        <button onClick={()=> {
+          setIsFinished(false);
+          setShowAnswer(true);
+          setActualQuestion(0);
+
+        }}>See Answers.</button>
+      </div>
+    </main>
+  )
+
+
+  if(showAnswer) return <main className="app"> 
+  
+  <div className="left-side">
+  <div className="question-number">
+    <span>Question {actualQuestion + 1} of {questions.length} </span> 
+  </div>
+  <div className="question-title">
+    {questions[actualQuestion].title}
+  </div>
+  <div>
+    {questions[actualQuestion].choices.filter((choice) => choice.isCorrect)[0].textResponse}
+  </div>
+  <button  onClick={() => {
+              if (actualQuestion === questions.length - 1) {
+                window.location.href = "/";
+              } else {
+               setActualQuestion(actualQuestion + 1);
+              }
+            }}> 
+                  {actualQuestion === questions.length - 1
+              ? "Play Again"
+              : "Next"}
+              </button>
+ </div>
+ </main>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="app">
+     <div className="left-side">
+      <div className="question-number">
+        <span>Question {actualQuestion + 1} of {questions.length} </span> 
+      </div>
+      <div className="question-title">
+        {questions[actualQuestion].title}
+      </div>
+      <div>
+        {!areDisabled ? (  <span className="remainTime">Remain Time : {isTimeRunning}</span>) : (
+          <button 
+          onClick={() =>{
+            setIsTimeRunning(10);
+            setAreDisabled(false);
+            setActualQuestion(actualQuestion + 1);
+          }}>Continue</button>
+        )}
+      
+      </div>
+     </div>
+     <div className="right-side">
+      {questions[actualQuestion].choices.map((response) => (
+        <button 
+        disabled={areDisabled}
+        key={response.textResponse} onClick={(e) => handleAnswerSubmit(response.isCorrect, e)}>{response.textResponse}</button>
+      ))}
+     </div>
+    </main>
   );
 }
 
---
+++ src/questions.js
@@ -0,0 +1,40 @@
+const questions =[
+{
+    title: "Who wrote the play 'Romeo and Juliet'?",
+    choices:[
+    { textResponse:"Charles Dickens" ,isCorrect: false},
+    { textResponse:"William Shakespeare" ,isCorrect: true},
+    { textResponse:"Jane Austen" ,isCorrect: false},
+    { textResponse:" Mark Twain" ,isCorrect:false},
+    ]
+},
+{
+    title: "Which film won the Academy Award for Best Picture in 2019?",
+    choices:[
+    { textResponse:"La La Land" ,isCorrect: false},
+    { textResponse:"The Shape of Water" ,isCorrect: false},
+    { textResponse:" Green Book" ,isCorrect: true},
+    { textResponse:" Parasite" ,isCorrect:false},
+    ]
+},
+{
+    title: "Which actress never play in an Almodovar film",
+    choices:[
+    { textResponse:"Penelope Cruz" ,isCorrect: false},
+    { textResponse:"Tilda Swindon" ,isCorrect: false},
+    { textResponse:" Rossy de Palma" ,isCorrect: false},
+    { textResponse:"Marivel Verdu" ,isCorrect:true},
+    ]
+},
+{
+    title: "How much is '11' + 1 in Javascript?",
+    choices:[
+    { textResponse:"111" ,isCorrect: true},
+    { textResponse:"12" ,isCorrect: false},
+    { textResponse:" Syntax Error" ,isCorrect: false},
+    { textResponse:"'11'1" ,isCorrect:false},
+    ]
+}
+
+]
+export default questions;
